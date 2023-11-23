d44f4f3d56cbc747df774ffe31fa46c61aa9165e
Probability wave <150269157+0011010110@users.noreply.github.com>
2023-11-23 23:04:21 +0800

    Add files via upload

---
+++ Grade judgement.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	float a;
+	int m, i,n;
+	printf("Please input the number of the students.\n");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		printf("Please input one student's grade.\n");
+		scanf("%f",&a);
+		m = (int)a / 10;
+		switch (m)
+		{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			printf("Grade E\n");
+			break;
+		case 6:
+			printf("Grade D\n");
+			break;
+		case 7:
+			printf("Grade C\n");
+			break;
+		case 8:
+			printf("Grade B\n");
+			break;
+		case 9:
+		case 10:
+			printf("Grade A\n");
+			break;
+		default:
+			printf("This Grade is invalid.\n");
+			break;
+		}
+	}
+}
